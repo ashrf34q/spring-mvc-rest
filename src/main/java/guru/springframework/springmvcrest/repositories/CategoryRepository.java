8653cab4d3018940b490a286908b535fb5409039
Ashraf Makki <makkiashraf08@gmail.com>
2022-08-30 12:52:56 -0400

    Initial commit

---
+++ src/main/java/guru/springframework/springmvcrest/repositories/CategoryRepository.java
@@ -0,0 +1,7 @@
+package guru.springframework.springmvcrest.repositories;
+
+import guru.springframework.springmvcrest.domain.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
